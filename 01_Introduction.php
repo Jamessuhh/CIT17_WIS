1a2547623a197ca297783b08b7c00c8ebc7e5680
Jamessuhh <jamesdy02@gmail.com>
2023-10-11 19:20:49 +0800

    Reviewing php

---
+++ 01_Introduction.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Hello World</title>
+</head>
+<body>
+    <?php echo "Hello, World!";?>
+</body>
+</html>
+
+<?php
+/*
+*<?php PHP code goes here ?>
+*<? PHP code goes here ?>
+*<script language="php"> PHP code goes here </script> 
+*/
+?>
